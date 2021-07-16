8b6dbe4a8f3efd5e256960fb8e3f3416cf135b95
Okeke-miles <franklinokeke9@gmail.com>
2021-07-16 15:07:46 -0400

    Changes

--- capstone-project/client/src/components/VideoList/VideoList.jsx
+++ capstone-project/client/src/components/VideoList/VideoList.jsx
@@ -4,12 +4,15 @@ import { NavLink } from "react-router-dom";
 import Search from "../Search/Search";
 import Fuse from "fuse.js"
 import "../VideoList/VideoList.scss"
-import Favourites from '../Favourites/Favourites';
+import WatchList from '../WatchList/WatchList';
+import RemoveWatchList from '../RemoveWatchList/RemoveWatchList';
 
 
 function VideoList (){
     const [ videoList, setVideoList] = useState([])
     const [searchValue, setSearchValue] = useState('');
+    const [watchList, setWatchList] = useState([]);
+
 
     useEffect(() => {
         axios.get(`/api/videos`)
@@ -25,24 +28,49 @@ function VideoList (){
         if (!pattern) {
           setVideoList(videoList);
           return;
-    }
+        }
 
-    const fuse = new Fuse(videoList, {
-        keys: ["title", "genre"],
-      });
-    
-    const result = fuse.search(pattern);
-
-      const matches = [];
-      if (!result.length) {
-        setVideoList([]);
-      } else {
-        result.forEach(({item}) => {
-          matches.push(item);
+        const fuse = new Fuse(videoList, {
+            keys: ["title", "genre"],
         });
-        setVideoList(matches);
-      }
-}
+        
+        const result = fuse.search(pattern);
+
+        const matches = [];
+        if (!result.length) {
+            setVideoList([]);
+        } else {
+            result.forEach(({item}) => {
+            matches.push(item);
+            });
+            setVideoList(matches);
+        }
+    }
+    useEffect(() => {
+        const newList = localStorage.getItem('react-app-watchList')
+        const videoWatchList = JSON.parse(newList);
+
+		setWatchList(videoWatchList);
+	}, []);
+
+    const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-app-watchList', JSON.stringify(items));
+	};
+
+    const addWatchListVideo = (video) => {
+		const newWatchList = [...watchList, video];
+		setWatchList(newWatchList);
+        saveToLocalStorage(newWatchList);
+	};
+
+    const removeWatchListVideo = (video) => {
+		const newWatchList = watchList.filter(
+			(item) => item.id !== video.id
+		);
+
+		setWatchList(newWatchList);
+        saveToLocalStorage(newWatchList);
+	};
 
     return(
         <>
@@ -58,13 +86,26 @@ function VideoList (){
                             key={video.id}
                             src={video.image} alt="video-thumbnail"/>
                         </NavLink>
-                        <div className='overlay d-flex align-items-center justify-content-center'>
-					    <Favourites />
+                        <div className='overlay d-flex align-items-center justify-content-center' onClick={() => addWatchListVideo(video)}>
+					    <WatchList />
 				        </div>
-                    </div>
-                        
+                    </div>  
                     ))}
                 </nav>
+                <p>MY WATCHLIST</p>
+                {watchList.map(video => (
+                    <div className='image-container d-flex justify-content-start m-3'>
+                        <NavLink to={`videos/${video.id}`}>
+                            <img className="thumbnail__style" 
+                            key={video.id}
+                            src={video.image} alt="video-thumbnail"/>
+                        </NavLink>
+                        <div className='overlay d-flex align-items-center justify-content-center' onClick={() => removeWatchListVideo(video)}>
+					    <RemoveWatchList />
+				        </div>
+                    </div>
+                ))}
+               
              
             </header>                      
         </div>
