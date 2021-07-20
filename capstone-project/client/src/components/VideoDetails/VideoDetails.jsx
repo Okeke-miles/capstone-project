05f8e52326c431a247e504c1cb1811833402a984
Okeke-miles <franklinokeke9@gmail.com>
2021-07-20 01:31:12 -0400

    Add and Edit endpoints done. Countdown timer done. Styling almost completed. MongoDB database setup.

--- capstone-project/client/src/components/VideoDetails/VideoDetails.jsx
+++ capstone-project/client/src/components/VideoDetails/VideoDetails.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
-import { Link, useParams } from "react-router-dom";
+import { BrowserRouter, Route, Link, useParams } from "react-router-dom";
+import editIcon from "../../assets/icon/edit-24px.svg"
+import "../VideoDetails/VideoDetails.scss"
+import EditShowing from "../EditShowing/EditShowing"
+import {DateTimePickerComponent} from "@syncfusion/ej2-react-calendars";
 
 function VideoDetails(){
 
     const [videoDetails, setVideoDetails] = useState(null);
+    // const [newShowing, setNewShowing] = useState(null)
     const { videoId } = useParams();
 
     useEffect(()=> {
@@ -16,8 +21,28 @@ function VideoDetails(){
                 console.log(err)
             })
     }, [videoId])
-    
-  
+
+    const updateShowing = (event) => {
+        event.preventDefault()
+        const newEdit = {showing: event.target.showing.value}
+        axios.put(`/api/videos/${videoId}`, newEdit
+        )
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+        axios.get(`/api/videos/${videoId}`)
+        .then(res => {
+            setVideoDetails(res.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    }
+
+
 
     if (videoDetails == null) {
         return <main>Loading...</main>
@@ -26,25 +51,34 @@ function VideoDetails(){
     const { title, channel, description, image, language, duration, year, genre, showing } = videoDetails
 
     return(
-        <main>
-            <h1>{title}</h1>
-            <img src={image} alt={title} />
-            <h2>Channel:</h2>
-            <p>{channel}</p>
-            <h2>Description:</h2>
-            <p>{description}</p>
-            <h2>Language:</h2>
-            <p>{language}</p>
-            <h2>Duration:</h2>
-            <p>{duration}</p>
-            <h2>Year:</h2>
-            <p>{year}</p>
-            <h2>Genre:</h2>
-            <p>{genre}</p>
-            <h2>Showing:</h2>
-            <p>{showing}</p>
-            <Link to="/videos"> Back to Video </Link>
+        <>
+         <Link className="back-link__style" to="/videos"> Back to Content </Link>
+        <main className="videodetails-container__style">
+            <img className="image__style" src={image} alt={title} />
+            <div className="details-desc__style">
+                <h1 className="desc-title__style" >{title}</h1>
+                <div className="channel__style">Channel: {channel}</div>
+                <div className="details-description__style">Description: {description}</div>
+                <div className="language__style">Language: {language}</div>
+                <div className="duration__style">Duration: {duration}</div>
+                <div className="year__style">Year: {year}</div>
+                <div className="genre__style">Genre: {genre}</div>
+                <div className="showing__style">Showing: {showing}                
+                </div>
+                <form onSubmit={(e)=>{
+                e.preventDefault()
+                // e.target.reset()
+                updateShowing(e)
+                }}>
+                    <label htmlFor="showing">Date</label>
+                    <input type="text" name= "showing"placeholder="new show time"/>
+                    <button type="submit" >Submit</button>
+                </form>
+                <DateTimePickerComponent placeholder="Choose a date and time"></DateTimePickerComponent>
+            </div>
         </main>
+       
+        </>
 
     )
 }
